d25547701d2f83a6d672275ac2539399006a4125
Алексей Морозов <almoroz08@gmail.com>
2024-01-02 15:48:20 +0300

    рабочий бэк для восстановления пароля

--- base/api/urls.py
+++ base/api/urls.py
@@ -9,7 +9,12 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('register/', views.register),
+
+    path('reset_password/', views.reset_password),
+    path('reset_password/<uidb64>/<token>/', views.check_password_tokens),
+
     path('polls/new/', views.create_poll, name='create_poll'),
     path('polls/', views.get_polls, name='view_polls'),
     path('polls/<int:pk>/', views.get_poll, name='poll-detail'),
@@ -19,13 +24,19 @@ urlpatterns = [
     path('polls/<int:pk>/results/', views.results, name='results'),
     path('users/<int:pk>/username/', views.get_author_name, name='get_author_name'),
     path('polls/<int:pk>/delete/', views.delete_poll, name='delete_poll'),
+
     path('complains/', views.get_complains, name='get_complains'),
     path('complains/<int:pk>/', views.get_complain, name='get_complain'),
+
     path('vote-history/', views.get_vote_history, name='get_vote_history'),
+
     path('settings/', views.get_user_data, name='get_user_data'),
     path('settings/edit/', views.edit_user_data, name='edit_user_data'),
+
     path('change_password/', views.change_password, name='change_password'),
+
     path('users/<int:pk>/', views.get_user, name='get_user'),
     path('user/delete/', views.delete_user, name='delete_user'),
+
     path('', views.getRoutes),
 ]
